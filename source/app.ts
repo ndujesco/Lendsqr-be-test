45be13959ce6d3a4adead994335910d12ef7a46d
ugo <ndujesco@users.noreply.github.com>
2024-03-29 09:39:49 +0100

    test that setup is working

--- source/app.ts
+++ source/app.ts
@@ -3,4 +3,6 @@ import { Application } from 'express';
 
 const app: Application = express();
 
+app.get('/', (req, res) => res.json({ message: 'Welcome!' }));
+
 export default app;
--- source/index.ts
+++ source/index.ts
@@ -1,1 +1,20 @@
 import app from './app';
+import { Application } from 'express';
+
+class Server {
+  private port = process.env.PORT || 3000;
+  private app;
+
+  constructor(app: Application) {
+    this.app = app;
+  }
+
+  start() {
+    this.app.listen(this.port, () => {
+      console.log(`Server listening on port ${this.port}`);
+    });
+  }
+}
+
+const server = new Server(app);
+server.start();

4a85a13a455616e8fa03a501e3004816c3c4a7c9
ugo <ndujesco@users.noreply.github.com>
2024-04-01 06:54:40 +0100

    change file name

--- source/app.ts
+++ source/app.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 import { Application } from 'express';
-import authRouter from './router/auth';
+import authRouter from './router/auth.router';
 import { ErrorHandler } from './middleware/error.middleware';
-import transactionRouter from './router/transaction';
-import userRouter from './router/user';
+import transactionRouter from './router/transaction.router';
+import userRouter from './router/user.router';
 
 const app: Application = express();
 

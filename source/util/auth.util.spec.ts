413eaf2c9b58608e689f3e4ac18d6269a83adee3
ugo <ndujesco@users.noreply.github.com>
2024-04-01 20:56:04 +0100

    update

---
+++ source/util/auth.util.spec.ts
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+
+import { createJWT } from './auth.util';
+import { UserI } from './interface.util';
+
+const userId = Math.floor(Math.random() * 1000);
+
+process.env = {
+  JWT_SECRET: '<jwt_secret>',
+  JWT_EXPIRATION_TIME: '2m',
+};
+
+console.log(process.env.JWT_SECRET);
+
+describe('returns a valid access token', () => {
+  it('should return the payload after decoding the token', () => {
+    const payload = { userId, email: '<email>' } as UserI;
+    const token = createJWT(payload);
+    const decode = jwt.verify(token, process.env.JWT_SECRET as string);
+    expect(decode).toMatchObject(payload);
+  });
+});

aa57833fd59342820b0a8c23e14285f85a996182
ugo <ndujesco@users.noreply.github.com>
2024-04-02 22:31:46 +0100

    refactor user.controller

--- source/router/user.router.ts
+++ source/router/user.router.ts
@@ -14,23 +14,6 @@ userRouter.get('/my/profile', protect, UserController.getMyProfile);
 userRouter.get('/wallet', protect, UserController.getMyBalance);
 
 userRouter.get(
-  '/profile/wallet',
-  protect,
-  UserController.getUserFromWalletNumber
-);
-
-userRouter.get('/profile/id', protect, UserController.getUserFromId);
-
-userRouter.get(
-  '/profile/by',
-  protect,
-  RequestValidator.validate(GetUserByDto, 'query'),
-  UserController.getUserBy
-);
-
-// user
-
-userRouter.get(
   '/transaction/type',
   protect,
   RequestValidator.validate(UserTransactionsDto, 'query'),
@@ -43,6 +26,21 @@ userRouter.get(
   '/transaction/common',
   protect,
   UserController.getCommonTransactions
+);
+
+userRouter.get(
+  '/profile/wallet',
+  protect,
+  UserController.getUserFromWalletNumber
+);
+
+userRouter.get('/profile/id', protect, UserController.getUserFromId);
+
+userRouter.get(
+  '/profile/by',
+  protect,
+  RequestValidator.validate(GetUserByDto, 'query'),
+  UserController.getUserBy
 );
 
 export default userRouter;

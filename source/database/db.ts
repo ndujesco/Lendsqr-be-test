0e30fe9dd9260f024dd84a9edd3cda7c490a940e
ugo <ndujesco@users.noreply.github.com>
2024-04-02 22:02:43 +0100

    remove redundant code

--- source/database/db.ts
+++ source/database/db.ts
@@ -1,8 +1,6 @@
 import knex from 'knex';
 
-import configs from './knexfile';
-
-const config = configs[process.env.NODE_ENV || 'development'];
+import { config } from './knexfile';
 
 const db = knex(config);
 
